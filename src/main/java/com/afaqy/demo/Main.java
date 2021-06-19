480c663de1e24085173cc3aead96aef063453f9a
mohammed-eladly-afaqy <mohammed.eladly@afaqy.com>
2021-06-19 17:02:11 +0200

    demo

---
+++ src/main/java/com/afaqy/demo/Main.java
@@ -0,0 +1,48 @@
+package com.afaqy.demo;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * Name : Main
+ * <p>
+ * Description :
+ * <p>
+ * Date : 16/06/2021
+ * <p>
+ * Create by : Mohammed ElAdly
+ * <p>
+ * Mail : mohammed.eladly@afaqy.com
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> textLines = builder.stream("streams-plaintext-input");
+        KTable<String, Long> wordCounts = textLines
+                .flatMapValues(textLine -> Arrays.asList(textLine.toLowerCase().split("\\W+")))
+                .groupBy((key, word) -> word)
+                .count(Materialized.as("counts"));
+        wordCounts.toStream().to("streams-wordcount-output", Produced.with(Serdes.String(), Serdes.Long()));
+
+        try(KafkaStreams streams = new KafkaStreams(builder.build(), props)) {
+            streams.start();
+        }
+    }
+
+}
